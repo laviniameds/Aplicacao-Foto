8d1b88d79714d7ddfd3fa72f149f449643b9a6ab
laviniameds <laviniameds@icloud.com>
2018-02-25 12:23:23 -0300

    ->criacao do .gitignore;

--- appFotovoltaica/urls.py
+++ appFotovoltaica/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^energia/', include('energia.urls')),
-]
+    
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
