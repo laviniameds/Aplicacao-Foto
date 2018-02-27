8d335d0a17ef8888b867bfc842ecf458b07ad188
laviniameds <laviniameds@icloud.com>
2018-02-27 20:52:33 -0300

    ->testes com formulario; ->dados passando do formulario para tabela;

--- energia/urls.py
+++ energia/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'$^', views.inserirDados),
+    url(r'^consumo_energia/$', views.consumoEnergia, name='consumo_energia'),
 ]
--- energia/views.py
+++ energia/views.py
@@ -2,3 +2,22 @@ from django.shortcuts import render
 
 def inserirDados(request):
     return render(request, "paginas/inserir_dados.html")
+
+def consumoEnergia(request):
+    data = {}
+    if request.method == 'POST':
+        data['consumo_ponta_qtd'] = request.POST.get('consumo_ponta_qtd', 'consumo_ponta_qtd not found')
+        data['consumo_ponta_valor'] = request.POST.get('consumo_ponta_valor', 'consumo_ponta_valor not found')
+        data['consumo_fora_qtd'] = request.POST.get('consumo_fora_qtd', 'consumo_fora_qtd not found')
+        data['consumo_fora_valor'] = request.POST.get('consumo_fora_valor', 'consumo_fora_valor not found')
+        data['tarifa'] = request.POST.get('tarifa', 'tarifa not found')
+        data['importe'] = request.POST.get('importe', 'importe not found')
+        data['valor_fatura'] = request.POST.get('valor_fatura', 'valor_fatura not found')
+    
+    return render(request, "paginas/consumo_energia.html", data)
+
+def despesaEnergia(request):
+    return render(request, "paginas/despesa_energia.html")
+
+def usina_fotovoltaica(request):
+    return render(request, "paginas/usina_fotovoltaica.html")
