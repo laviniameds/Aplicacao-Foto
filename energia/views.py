a8f862f5b63362eb21d6cfc8cf68103c88cf2dae
Lavinia Medeiros Miranda <laviniameds@icloud.com>
2018-04-14 11:59:44 -0300

    ->atualização das formulas de cada campo da tabela na pag de views;

--- energia/views.py
+++ energia/views.py
@@ -11,13 +11,40 @@ def consumoEnergia(request):
         data['consumo_ponta_valor'] = request.POST.get('consumo_ponta_valor', 'consumo_ponta_valor not found')
         data['consumo_fora_qtd'] = request.POST.get('consumo_fora_qtd', 'consumo_fora_qtd not found')
         data['consumo_fora_valor'] = request.POST.get('consumo_fora_valor', 'consumo_fora_valor not found')
+        data['consumo_fora_preco'] = request.POST.get('consumo_fora_preco', 'consumo_fora_preco not found')
         data['tarifa'] = request.POST.get('tarifa', 'tarifa not found')
+        data['injetado_fv'] = request.POST.get('injetado_fv', 'injetado_fv not found')
         data['importe'] = request.POST.get('importe', 'importe not found')
+        data['tributo_federal'] = request.POST.get('tributo_federal', 'tributo_federal not found')       
         data['valor_fatura'] = request.POST.get('valor_fatura', 'valor_fatura not found')
     
-    """valores['cosern'] = int(data['consumo_ponta_qtd']) + int(data['consumo_fora_qtd'])
-    valores['consumido_fv'] = 0
-    valores['injetado_fv'] = 0
-    valores['total_EA'] = valores['cosern'] + valores['consumido_fv'] + valores['injetado_fv']"""
+    """ FORMULAS DE CADA CAMPO8:
+
+    GERACAO_DA_USINA = SUNNY PORTAL
+
+    TABELA DE CONSUMO DE ENERGIA ATIVA
+
+    valores['cosern'] = int(data['consumo_ponta_qtd']) + int(data['consumo_fora_qtd'])
+    valores['injetado_fv'] = int(data['injetado_fv'])
+    valores['consumido_fv'] = (GERACAO_DA_USINA * int(data['tarifa']) / int(data['tarifa'])
+    valores['total_EA'] = valores['cosern'] + valores['consumido_fv'] + valores['injetado_fv']
+
+    TABELA DE DESPESA DE ENERGIA ATIVA
+
+    valores['tarifa'] = int(data['tarifa'])
+    valores['com_fv'] = int(data['consumo_ponta_valor']) + int(data['consumo_fora_valor']) + int(data['importe'])
+    valores['sem_fv'] = (int(data['consumo_ponta_valor']) + int(data['consumo_fora_valor']) + int(data['injetado_fv']) +
+                        valores['consumido_fv']) * int(data['consumo_fora_preco'])  
+    valores['valor_bruto'] = int(data['valor_fatura']) - int(data['tributo_federal'])
+    valores['economia'] = valores['sem_fv'] - valores['sem_fv']
+
+    TABELA DA USINA FOTOVOLTAICA
+
+    valores['geracao'] = GERACAO_DA_USINA
+    valores['total_UF'] = GERACAO_DA_USINA * int(data['tarifa']
+    valores['injetado_UF'] = int(data['tarifa'] * valores['injetado_fv']
+    valores['consumido_UF'] = valores['total_UF'] - GERACAO_DA_USINA
+    
+    """
 
     return render(request, "paginas/view_tabelas.html", valores)
