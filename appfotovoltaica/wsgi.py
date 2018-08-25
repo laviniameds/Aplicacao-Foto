e803476a2ea7208697c1d194749aff57df2f52af
Tiago Lisboa <tiago.caio.ol@gmail.com>
2018-08-24 22:13:22 -0300

    Mudando para o docker

--- appfotovoltaica/wsgi.py
+++ appfotovoltaica/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for appFotovoltaica project.
+WSGI config for appfotovoltaica project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "appFotovoltaica.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "appfotovoltaica.settings")
 
 application = get_wsgi_application()
